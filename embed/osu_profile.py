822100987f3032db089b09b489a08bd1d87e2436
Toby <kommandertoby@gmail.com>
2021-11-12 01:19:28 +0000

    -osu command implementation

--- embed/osu_profile.py
+++ embed/osu_profile.py
@@ -14,7 +14,10 @@ class Profile():
             title=("osu! profile of " + dict['username']),
             color=discord.Colour.red()
         )
-        embed.description = "gonna do this after i know it actually sends an embed"
+        embed.description = "**Rank**: " + str(dict['statistics']['global_rank']) + "\n" + \
+                            "**Country Rank**: " + str(dict['statistics']['country_rank']) + "\n" + \
+                            "**Accuracy**: " + \
+                            str(round(dict['statistics']['hit_accuracy'], 2)) + "%"
         embed.set_author(name='Snipebot 3 by Komm',
                         icon_url="https://osu.ppy.sh/images/flags/" + dict['country_code']+".png")
         if dict['avatar_url'][0] == "/":
@@ -22,4 +25,6 @@ class Profile():
                                 str(dict['avatar_url']))
         else:
             embed.set_thumbnail(url=str(dict['avatar_url']))
+        if dict['cover_url']:
+            embed.set_image(url=str(dict['cover_url']))
         await ctx.send(embed=embed)

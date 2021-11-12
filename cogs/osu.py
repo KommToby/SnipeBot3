73623b673aa0631e23e7ced7b79b5a24fe2fe5e4
hjn2g19 <hjn2g19@soton.ac.uk>
2021-11-12 02:55:02 +0000

    removed unnecessary osu auth stuff (should only need 1 per bot, now generates auth in osu file) renamed osu commands file so it doesnt conflict

--- cogs/osu.py
+++ cogs/osu.py
@@ -1,20 +1,21 @@
-from embed.osu_profile import Profile
-import discord
+from embed.osu_profile import create_osu_profile_embed
+from osu.osu import Osu
 from discord.ext import commands
 
 import sys
 sys.path.append("..")
 
 
-class Osu(commands.Cog):  # must have commands.cog or this wont work
-
+class OsuCommands(commands.Cog):  # must have commands.cog or this wont work
     def __init__(self, client):
         self.client = client
-        self.profile = Profile()
+        self.osu = Osu()
 
     @commands.command(aliases=['profile'])
-    async def osu(self, ctx, id: str):
-        await self.profile.profile(ctx, id)
+    async def osu(self, ctx, user: str):
+        user_data = await self.osu.get_user_data(user)
+        embed = create_osu_profile_embed(user_data)
+        await ctx.send(embed=embed)
 
     @osu.error
     async def on_command_error(self, ctx, error):
@@ -23,4 +24,4 @@ class Osu(commands.Cog):  # must have commands.cog or this wont work
 
 
 def setup(client):
-    client.add_cog(Osu(client))
+    client.add_cog(OsuCommands(client))
--- embed/osu_profile.py
+++ embed/osu_profile.py
@@ -1,30 +1,26 @@
-from osu.osu_auth import OsuAuth
 import discord
 import sys
 sys.path.append("..")
 
 
-class Profile():
-    def __init__(self):
-        self.auth = OsuAuth()
+def create_osu_profile_embed(profile_info):
+    embed = discord.Embed(
+        title=("osu! profile of " + profile_info['username']),
+        color=discord.Colour.red()
+    )
+    embed.description = \
+f"""**Rank**: {profile_info['statistics']['global_rank']}
+**Country Rank**: {profile_info['statistics']['country_rank']}
+**Accuracy**: {round(profile_info['statistics']['hit_accuracy'], 2)}%
+"""
+    embed.set_author(name="Snipebot 3 by Komm",
+                     icon_url=f"https://osu.ppy.sh/images/flags/{profile_info['country_code']}.png")
+    if profile_info['avatar_url'][0] == "/":
+        embed.set_thumbnail(url="https://osu.ppy.sh" +
+                            str(profile_info['avatar_url']))
+    else:
+        embed.set_thumbnail(url=str(profile_info['avatar_url']))
+    if profile_info['cover_url']:
+        embed.set_image(url=str(profile_info['cover_url']))
+    return embed
 
-    async def profile(self, ctx, userid: str):
-        dict = await self.auth._get_api_v2("/v2/users/" + str(userid), ctx)
-        embed = discord.Embed(
-            title=("osu! profile of " + dict['username']),
-            color=discord.Colour.red()
-        )
-        embed.description = "**Rank**: " + f"{(dict['statistics']['global_rank']):,}" + "\n" + \
-                            "**Country Rank**: " + f"{(dict['statistics']['country_rank']):,}" + "\n" + \
-                            "**Accuracy**: " + \
-                            str(round(dict['statistics']['hit_accuracy'], 2)) + "%"
-        embed.set_author(name='Snipebot 3 by Komm',
-                        icon_url="https://osu.ppy.sh/images/flags/" + dict['country_code']+".png")
-        if dict['avatar_url'][0] == "/":
-            embed.set_thumbnail(url="https://osu.ppy.sh" +
-                                str(dict['avatar_url']))
-        else:
-            embed.set_thumbnail(url=str(dict['avatar_url']))
-        if dict['cover_url']:
-            embed.set_image(url=str(dict['cover_url']))
-        await ctx.send(embed=embed)

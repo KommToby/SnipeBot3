c543a692c1d796b5e529a58f32d9c23ddccada61
Toby <kommandertoby@gmail.com>
2021-11-14 11:34:08 +0000

    Fix bug when user hasnt got any snipes

--- cogs/snipes.py
+++ cogs/snipes.py
@@ -20,9 +20,12 @@ class Snipes(commands.Cog): # must have commands.cog or this wont work
         sniped = self.database.get_user_snipes(main_user_id, str(user_data['id']))
         total_snipes = self.database.get_total_snipes(main_user_id)
         user_snipes = self.database.get_user_snipes(user_data['id'], main_user_id)
-        random_play_index = random.randint(0, len(user_snipes)-1)
-        random_play = user_snipes[random_play_index]
-        random_play_data = await self.osu.get_score_data(random_play[1], random_play[0])
+        if user_snipes != []:
+            random_play_index = random.randint(0, len(user_snipes)-1)
+            random_play = user_snipes[random_play_index]
+            random_play_data = await self.osu.get_score_data(random_play[1], random_play[0])
+        else:
+            random_play_data = False
         embed = await create_snipes_embed(user_data, snipes, sniped, total_snipes, random_play_data)
         await ctx.send(embed=embed)
 
